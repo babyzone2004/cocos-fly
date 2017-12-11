d89a133389bac8c0570c1221cfa20cf9e2e7b708
yangweixian <yangweixian@kuaikanmanhua.com>
2017-12-11 14:34:22 +0800

    update

--- gulpfile.js
+++ gulpfile.js
@@ -4,12 +4,8 @@ var htmlmin = require("gulp-htmlmin");
 var rev = require("gulp-rev");
 var imagemin = require("gulp-imagemin");
 var revCollector = require("gulp-rev-collector");
-var urlPrefixer = require("gulp-url-prefixer");
-var uglify = require("gulp-uglify");
 var del = require("del");
-var fs = require("fs");
 var javascriptObfuscator = require("gulp-javascript-obfuscator");
-var mangleModuleName = require("gulp-mangle-modules-name");
 
 gulp.task("cp-src", function (cb) {
     gulp.src(["./build/web-mobile/src/**"])
@@ -26,43 +22,7 @@ gulp.task("imagemin", ["cp-src"], function (cb) {
         .pipe(gulp.dest("./build/web-mobile/"))
         .on("end", cb);
 });
-gulp.task("prefix-css", ["imagemin"], function (cb) {
-    gulp.src(["./build/web-mobile/style-mobile.css"])
-        // .pipe(urlPrefixer.css({
-        //     prefix: "http://yp_cdn.zz-game.com"
-        // }))
-        .pipe(gulp.dest("./build/web-mobile/")
-            .on("end", cb));
-});
-
-// 混淆
-gulp.task("mangle", ["prefix-css"], function (cb) {
-    gulp.src(["./build/web-mobile/**/*.js", "./build/web-mobile/**/*.json"])
-        .pipe(mangleModuleName(["ItemList", "ItemTemplate", "BackPackUI", "ButtonScaler", "ChargeUI", "EnergyCounter", "HeroSlot", "HomeUI", "PanelTransition", "ShopUI", "SubBtnsUI", "MainMenu", "MenuSidebar", "TabCtrl"]))
-        .pipe(gulp.dest("./build/web-mobile")
-            .on("end", cb));
-});
-gulp.task("optimize", ["mangle"], function (cb) {
-    var mangleProp = JSON.parse(fs.readFileSync("./mangle_prop.json", "utf8"));
-    gulp.src([ "./build/web-mobile/cocos2d-js-min.js", "./build/web-mobile/main.js", "./build/web-mobile/settings.js", "./build/web-mobile/project.js"])
-        .pipe(uglify({
-            mangle: {
-                toplevel: true,
-                properties: {
-                    regex: new RegExp("^" + mangleProp.join("$|^") + "$")
-                }
-            },
-            compress: {
-                toplevel: true,
-                passes: 3
-            },
-            toplevel: true,
-            nameCache: {}
-        }))
-        .pipe(gulp.dest("./build/web-mobile")
-            .on("end", cb));
-});
-gulp.task("htmlmin", ["optimize"], function (cb) {
+gulp.task("htmlmin", ["imagemin"], function (cb) {
     gulp.src("./build/web-mobile/*.html")
         .pipe(fileInline())
         .pipe(htmlmin({
@@ -112,9 +72,6 @@ gulp.task("default", ["resRev"], function (cb) {
         .pipe(revCollector({
             replaceReved: true
         }))
-        // .pipe(urlPrefixer.html({
-        //     prefix: "http://yp_cdn.zz-game.com"
-        // }))
         .pipe(gulp.dest("./build/web-mobile/")
             .on("end", cb));
 });
